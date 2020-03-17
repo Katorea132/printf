5ee3e7fa878d24029820d59c0f637a931e862957
Katorea132 <katorea132@gmail.com>
2020-03-16 22:06:24 -0500

    Added the printf function

---
+++ _prinft.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * _printf - Prints with formats to screen
+ * @format: Holds such formats
+ * @...: Many more arguments :D
+ * Return: -1 on failure, number of printed chars on success
+ */
+int _printf(const char *format, ...)
+{
+	va_list arg;
+	unsigned int i, j, cnt = 0, flag;
+	const unsigned int BUFF = 1024;
+	char *buffer = malloc(sizeof(char) * BUFF);
+	frm form[] = {
+		{"c", handc}, {"s", hands}, {"d", handd}, {"i", handi}, {"b", handb},
+		{"S", handS}, {"r", handr}, {"R", handR}, {"x", handx}, {"X", handX},
+		{"o", hando}, {"u", handu}, {"p", handp}, {0, 0}
+	};
+
+	if (format == 0 || (format[0] == '%' && format[1] == 0))
+		return (-1);
+	va_start(arg, format);
+	for (i = 0; format[i] != 0; i++)
+	{
+		if (format[i] == '%' && format[i + 1] == '%')
+			_putchar('%'), i++, cnt += 1;
+		else if (format[i] == '%')
+		{
+			for (j = 0, flag = 0; form[j].op != 0; j++)
+				if (format[i + 1] == form[j].op[0])
+					cnt += form[j].p(arg), flag = 1, i++;
+			if (flag == 0)
+				_putchar(format[i]), cnt += 1;
+		}
+		else
+			_putchar(format[i]), cnt += 1;
+	}
+	va_end(arg), free(buffer);
+	return (cnt);
+}
