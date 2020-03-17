98c66bed9ca032e95cb7ea04b985b248e686188e
Katorea132 <katorea132@gmail.com>
2020-03-17 11:08:10 -0500

    Attempt at fix

--- _prinft.c
+++ _prinft.c
@@ -8,35 +8,34 @@
 int _printf(const char *format, ...)
 {
 	va_list arg;
-	unsigned int i, j, cnt = 0, flag, b = 1;
+	unsigned int i, j, cnt = 0, flag;
 	frm form[] = {
 		{"c", handc}, {"s", hands}, {"d", handi}, {"i", handi}, {"b", handb},
 		{"S", handS}, {"r", handr}, {"R", handR}, {"x", handx}, {"X", handX},
 		{"o", hando}, {"u", handu}, {"p", handp}, {0, 0}
 	};
 
-	if (format == 0)
+	if (format == 0 || (format[0] == '%' && format[1] == 0))
 		return (-1);
 	va_start(arg, format);
-	for (i = 0; format[i] != 0;)
-	{	if (format[i] == '%' && format[i + b] == 0)
+	for (i = 0; format[i] != 0; i++)
+	{
+		if (format[i] == '%' && format[i + 1] == 0)
 			return (-1);
-		else if (format[i] == '%' && format[i + b] == ' ')
-			b++;
-		else  if (format[i] == '%' && format[i + b] == '%')
-			_putchar('%'), i++, cnt += 1, b = 1;
+		else  if (format[i] == '%' && format[i + 1] == '%')
+			_putchar('%'), i++, cnt += 1;
 		else if (format[i] == '%')
 		{
-			if (format[i + b] == 0)
+			if (format[i + 1] == 0)
 				return (-1);
 			for (j = 0, flag = 0; form[j].op != 0; j++)
-				if (format[i + b] == form[j].op[0])
-					cnt += form[j].p(arg), flag = 1, i += b + 1, b = 1;
+				if (format[i + 1] == form[j].op[0])
+					cnt += form[j].p(arg), flag = 1, i++;
 			if (flag == 0)
-				_putchar(format[i]), cnt += 1, i++;
+				_putchar(format[i]), cnt += 1;
 		}
 		else
-			_putchar(format[i]), cnt += 1, i = i + b;
+			_putchar(format[i]), cnt += 1;
 	}
 	va_end(arg);
 	return (cnt);
